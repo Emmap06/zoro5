18f5dc88e278f4f533cd95e82ffc797c5f33d5d3
AboubacarSiriki <sidiksoum344@gmail.com>
2023-11-02 11:06:48 +0000

    FIN DE PROJET
    FIN DE PROJET

--- produit/app.py
+++ produit/app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template,request, url_for, redirect,flash
+from flask_mysql_connector import MySQL
+import MySQLdb.cursors
+
 
 app = Flask(__name__)
+
+app.secret_key = 'message'  # Remplacez par une clé secrète sécurisée
+
+
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DATABASE'] = "gestion_magasin"
+
+mysql = MySQL(app)
 
 @app.route('/')
 def first():
@@ -16,23 +29,68 @@ def sixth():
 
 @app.route('/list_magasin/')
 def second():
-    return(render_template("list_magasin.html"))
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM magasin")
+    listes = cursor.fetchall()
+    cursor.close()
+    return render_template('list_magasin.html', listes=listes)
 
-@app.route('/ajout_mag/')
+@app.route('/ajout_mag', methods=['POST', 'GET'])
 def third():
-    return(render_template("ajout_mag.html"))
+
+    if request.method == 'POST':
+
+        nom = request.form['nom']
+        adresse = request.form['adresse']
+        telephone = request.form['telephone']
+        email = request.form['email']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor )
+        cursor.execute("INSERT INTO magasin (nomMagasin,adresse,telephone,email) VALUES (%s, %s,%s, %s)", (nom,adresse,telephone,email))
+        mysql.connection.commit()
+        cursor.close()
+        return redirect(url_for("fourth"))
+    flash('Modification réussie !', 'success')
+    return render_template("ajout_mag.html")
 
 @app.route('/enregistrement_mag/')
 def fourth():
-    return(render_template("enregistrement_mag.html"))
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM magasin")
+    listes = cursor.fetchall()
+    cursor.close()
+    return render_template('enregistrement_mag.html', listes=listes)
 
-@app.route('/modif_magasin/')
-def fifth1():
-    return(render_template("modif_magasin.html"))
+@app.route('/modif_magasin/<int:id>', methods=['GET', 'POST'])
+def fifth1(id):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    requete = "SELECT * FROM magasin WHERE magasinid = %s"
+    cursor.execute(requete, (id,))
+    elements = cursor.fetchone()
+    cursor.close()
+
+    if request.method == 'POST':
+        id = request.form['id']
+        nouveau_nom = request.form['nom']
+        nouvel_adresse = request.form['adresse']
+        nouveau_telephone = request.form['telephone']
+        nouvel_email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("UPDATE magasin SET MagasinID=%s,  nomMagasin = %s, adresse = %s, telephone = %s, email = %s WHERE magasinID = %s", (id,nouveau_nom, nouvel_adresse,nouveau_telephone,nouvel_email, id))
+        mysql.connection.commit()
+        cursor.close()
+
+        return redirect(url_for('fifth2'))
+
+    return(render_template("modif_magasin.html", elements=elements))
 
 @app.route('/mag_modifie/')
 def fifth2():
-    return(render_template("mag_modifie.html"))
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM magasin")
+    listes = cursor.fetchall()
+    cursor.close()
+    return render_template('mag_modifie.html', listes=listes)
 
 @app.route('/sup_magasin/')
 def fifth3():
@@ -40,7 +98,11 @@ def fifth3():
 
 @app.route('/effec_magasin/')
 def fifth4():
-    return(render_template("effec_magasin.html"))
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM magasin")
+    listes = cursor.fetchall()
+    cursor.close()
+    return render_template('effec_magasin.html', listes=listes)
 
 @app.route('/list_produit/')
 def sixth2():
@@ -62,9 +124,21 @@ def nineth():
 def tenth():
     return(render_template("produit_modifie.html"))
 
-@app.route('/valid_sup/')
-def eleventh():
-    return(render_template("valid_sup.html"))
+@app.route('/valid_sup/<int:id>', methods=['GET', 'POST'])
+def eleventh(id):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM magasin WHERE magasinID = %s", (id,))
+    elements = cursor.fetchone()
+    cursor.close()
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("DELETE FROM magasin WHERE magasinid = %s", (id,))
+        mysql.connection.commit()
+        cursor.close()
+        return redirect(url_for('fifth4'))
+
+    return render_template("valid_sup.html", elements=elements)
+
 
 @app.route('/effectue/')
 def twelveth():
